fcb5052205897395e5ad16f0196783e2f4f50b8e
DANIEL VITOR <daniel.vitor@primitecnologia.local>
2022-02-08 16:29:27 -0300

    start project

---
+++ index.js
@@ -0,0 +1,9 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import {Tarot} from './Tarot';
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => Tarot);
